056d56bbdd7a37e446658b4991196c9129824568
vincentwang <vincentwang@umail.ucsb.edu>
2017-11-07 00:57:46 -0800

    Code cleanup

--- wall-e/find_chessboard_corners.py
+++ wall-e/find_chessboard_corners.py
@@ -1,112 +1,119 @@
 import cv2
 import numpy as np
-import pprint
 
 # This is helpful: 'help(cv2.stereoRectify)' this will give you info on whatever function you want
+
+# deinterlace video with ffmpeg first
+
+# get right image from video
+vc_obj_right = cv2.VideoCapture("../Right.ASF")
+frame_idx_to_capture = 710
+vc_obj_right.set(cv2.CAP_PROP_POS_FRAMES, frame_idx_to_capture)
+vc_obj_right_success, img_right = vc_obj_right.read()
+
+# flip image and show both
+cv2.flip(img_right, -1, img_right)
+img_left = cv2.imread('image.jpg')
+cv2.imshow('left', img_left)
+cv2.imshow('right', img_right)
+
+# convert to gray scale
+img_left = cv2.cvtColor(img_left, cv2.COLOR_BGR2GRAY)
+img_right = cv2.cvtColor(img_right, cv2.COLOR_BGR2GRAY)
+
+# For the 3rd argument, removing those parameters seems to have no effect
+img_left_corners_success, img_left_corner_coords = cv2.findChessboardCorners(img_left, (5, 4),
+                                                                             cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FILTER_QUADS)
+img_right_corners_success, img_right_corner_coords = cv2.findChessboardCorners(img_right, (5, 4),
+                                                                               cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FILTER_QUADS)  # 7,5 works
+
+print "Detected corners in left image:\t\t" + str(img_right_corners_success)
+print "Detected corners in right image:\t" + str(img_left_corners_success)
+
+# Tuning these parameters does not appear to effect the end result
+max_iterations = 30
+epsilon = 0.0001
+criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, max_iterations, epsilon)
 
 # Create (x,y,z) object points for all intersections on checkerboard grid
 objp = np.zeros((5 * 4, 3), np.float32)
 objp[:, :2] = np.mgrid[0:5, 0:4].T.reshape(-1, 2)
 
-
-# print objp this may need adjustment
-
-# Create empty arrays for oject points and image points from each image
+# Create empty arrays for object points and image points from each image
 objpoints = []
-imgpoints_l = []
-imgpoints_r = []
-
-
-# Tuning these parameters does not appear to effect the end result
-criteria = (cv2.TERM_CRITERIA_COUNT + cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.0001)
-
-# deinterlace video with ffmpeg first
-
-# get right image from video
-r = cv2.VideoCapture("../Right.ASF")
-r.set(1, 710)
-succ, img_r = r.read()
-
-# flip image and show both
-cv2.flip(img_r,-1,img_r)
-img_l = cv2.imread('image.jpg')
-cv2.imshow('left',img_l)
-cv2.imshow('right',img_r)
-
-# convert to gray scale
-img_l = cv2.cvtColor(img_l,cv2.COLOR_BGR2GRAY)
-img_r = cv2.cvtColor(img_r,cv2.COLOR_BGR2GRAY)
-
-# cv2.waitKey(0)
-
-# For the 3rd argument, removing those parameters seems to have no effect
-ret, corners_l = cv2.findChessboardCorners(img_l,(5,4), cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FILTER_QUADS)
-ret_r, corners_r = cv2.findChessboardCorners(img_r,(5,4), cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FILTER_QUADS) # 7,5 works
-print ret_r
-print ret
-
+refined_img_points_left = []
+refined_img_points_right = []
 
 # If found, add object points to objpoints array, and add image points (after refining them) to imgpoints array
-if ret and ret_r:
+if img_left_corners_success and img_right_corners_success:
     objpoints.append(objp)
-    # tuning these params does nothing
-    corners_l = cv2.cornerSubPix(img_l, corners_l, (11, 11), (-1, -1), criteria)  # refine image points
-    corners_r = cv2.cornerSubPix(img_r, corners_r, (11, 11), (-1, -1), criteria)  # refine image points
 
-    # append image points (think of them as (x,y) in the picture
-    imgpoints_l.append(corners_l)
-    imgpoints_r.append(corners_r)
+    # cornerSubPix refines the corner coordinates. (tuning these params does nothing)
+    img_left_corner_coords = cv2.cornerSubPix(img_left, img_left_corner_coords, (11, 11), (-1, -1), criteria)
+    img_right_corner_coords = cv2.cornerSubPix(img_right, img_right_corner_coords, (11, 11), (-1, -1), criteria)
 
+    # append image points (think of them as (x,y) in the picture)
+    refined_img_points_left.append(img_left_corner_coords)
+    refined_img_points_right.append(img_right_corner_coords)
 
-    # Draw and display the corners
-    # cv2.drawChessboardCorners(img_l, (5, 4), corners_l, ret)
-    # cv2.drawChessboardCorners(img_r, (5, 4), corners_l, ret)
-    # cv2.drawChessboardCorners(img_l, (5, 4), corners_r, ret_r)
-    # cv2.drawChessboardCorners(img_r, (5, 4), corners_r, ret_r)
-    # cv2.imshow('Image with Corner Points_l', img_l)
-    # cv2.imshow('Image with Corner Points_r', img_r)
-    # cv2.waitKey(0)
+reprojection_error_left, cam_matrix_left, distortion_coeff_left, rotation_vec_left, translation_vec_left = cv2.calibrateCamera(
+    objpoints,
+    refined_img_points_left,
+    img_left.shape[::-1],
+    None, None)
+reprojection_error_right, cam_matrix_right, distortion_coeff_right, rotation_vec_right, translation_vec_right = cv2.calibrateCamera(
+    objpoints,
+    refined_img_points_right,
+    img_right.shape[::-1],
+    None, None)
 
+# https://docs.opencv.org/3.1.0/d9/d0c/group__calib3d.html#ga246253dcc6de2e0376c599e7d692303a
+img_left_corners_success, cam_matrix_left, distortion_coeff_left, cam_matrix_right, distortion_coeff_right, R, T, E, F = cv2.stereoCalibrate(
+    objpoints,
+    refined_img_points_left,
+    refined_img_points_right,
+    cam_matrix_left, distortion_coeff_left, cam_matrix_right, distortion_coeff_right,
+    img_right.shape[::-1],
+    flags=cv2.CALIB_FIX_PRINCIPAL_POINT)
 
-
-retval, mtx_l, dst_l, rvecs_l, tvecs_l = cv2.calibrateCamera(objpoints,imgpoints_l,img_l.shape[::-1],None,None)
-retval, mtx_r, dst_r, rvecs_r, tvecs_r = cv2.calibrateCamera(objpoints,imgpoints_r,img_r.shape[::-1],None,None)
-
-
-
-
-ret, mtx_l, dst_l, mtx_r, dst_r, R, T, E, F = cv2.stereoCalibrate(objpoints,imgpoints_l,imgpoints_r,
-                                                                  mtx_l, dst_l, mtx_r, dst_r, img_r.shape[::-1],
-                                                                  flags=cv2.CALIB_FIX_PRINCIPAL_POINT)
-
-
-h, w = img_l.shape
-
-# mtx_lN, roi_l = cv2.getOptimalNewCameraMatrix(mtx_l, dst_l, (w, h), 1, (w, h))
-# mtx_rN, roi_r = cv2.getOptimalNewCameraMatrix(mtx_r, dst_l, (w, h), 1, (w, h))
-
-# undistort, this appears to do nothing
-
+h, w = img_left.shape
 
 # stereo rectify
-R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(mtx_l, dst_l, mtx_r, dst_r,
-                                                                  img_r.shape[::-1], R, T, alpha=0.90)
+R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(cam_matrix_left,
+                                                                  distortion_coeff_left,
+                                                                  cam_matrix_right,
+                                                                  distortion_coeff_right,
+                                                                  img_right.shape[::-1],
+                                                                  R, T, alpha=0.90)
 
 # new_shape = (1280//2,960//2)
-map_l = cv2.initUndistortRectifyMap(mtx_l,dst_l, R1, P1, img_r.shape[::-1], cv2.CV_32F)
-map_r = cv2.initUndistortRectifyMap(mtx_r,dst_r, R2, P2, img_r.shape[::-1], cv2.CV_32F)
+map_l = cv2.initUndistortRectifyMap(cam_matrix_left,
+                                    distortion_coeff_left,
+                                    R1, P1,
+                                    img_right.shape[::-1],
+                                    cv2.CV_32F)
+map_r = cv2.initUndistortRectifyMap(cam_matrix_right,
+                                    distortion_coeff_right,
+                                    R2, P2,
+                                    img_right.shape[::-1],
+                                    cv2.CV_32F)
 
+img_left = cv2.remap(img_left,
+                     map_l[0],
+                     map_l[1],
+                     cv2.INTER_LANCZOS4)
+img_right = cv2.remap(img_right,
+                      map_r[0],
+                      map_r[1],
+                      cv2.INTER_LANCZOS4)
 
-img_l = cv2.remap(img_l,map_l[0],map_l[1],cv2.INTER_LANCZOS4)
-img_r = cv2.remap(img_r,map_r[0],map_r[1],cv2.INTER_LANCZOS4)
+for line in range(0, int(img_left.shape[0] / 20)):
+    img_left[line * 20, :] = 255
+    img_right[line * 20, :] = 255
 
-for line in range(0, int(img_l.shape[0] / 20)):
-    img_l[line * 20, :] = 255
-    img_r[line * 20, :] = 255
-
-
-cv2.imshow('stereo rectified_l',img_l)
-cv2.imshow('stereo rectified_r', img_r)
-
+cv2.imshow('Left Image - Stereorectified', img_left)
+print "Showing left image (stereorectified)"
+cv2.imshow('Right Image - Stereorectified', img_right)
+print "Showing right image (stereorectified)"
 
 cv2.waitKey(0)
