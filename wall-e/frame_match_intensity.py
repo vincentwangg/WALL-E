e707e5e10d299d3346bade0c8f9ea7ee196be2b3
Karthik Kribakaran <kkribakaran@gmail.com>
2017-11-17 08:32:33 -0800

    added intensity curve plotting functionality

--- wall-e/frame_match_intensity.py
+++ wall-e/frame_match_intensity.py
@@ -1,6 +1,8 @@
 import numpy as np
+import time
 import sys
 import cv2
+import matplotlib.pyplot as plt
 
 def get_gradient_diff(l_gradient, r_gradient, offset):
   gradient_len = len(l_gradient)
@@ -18,38 +20,47 @@ def get_optimal_offset(l_gradient, r_gradient, max_offset):
       optimal_offset = offset
   return optimal_offset
 
-def calculate_gradient(file_name, start_frame, num_frames):
+def calculate_gradient(file_name):
   feed = cv2.VideoCapture(file_name)
 
-  feed.set(cv2.CAP_PROP_POS_FRAMES, start_frame);
   prev_frame = cv2.cvtColor(feed.read()[1], cv2.COLOR_BGR2GRAY)
   x, prev_frame = cv2.threshold(prev_frame, 150, 255, cv2.THRESH_BINARY)
-  total_count = float(feed.get(cv2.CAP_PROP_FRAME_COUNT))
+  num_frames = int(feed.get(cv2.CAP_PROP_FRAME_COUNT))
   idx = 0
-  gradient = np.empty(num_frames)
-  while idx < num_frames and feed.isOpened():
-      prog = 100 * idx / (num_frames - 1)
+  gradient = np.empty(num_frames - 1)
+  while idx < num_frames - 1 and feed.isOpened():
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
+      prog = 100 * idx / (num_frames - 2)
       sys.stdout.write('\r{0}% - [{1}{2}]'.format(prog, '*' * prog, ' ' * (100 - prog)))
       sys.stdout.flush()
       frame = cv2.cvtColor(feed.read()[1], cv2.COLOR_BGR2GRAY)
       x, frame = cv2.threshold(frame, 150, 255, cv2.THRESH_BINARY)
       gradient[idx] = np.sum(abs((frame - prev_frame)))
       idx = idx + 1
-      if cv2.waitKey(1) & 0xFF == ord('q'):
-          break
   feed.release()
   return gradient
 
+def plot_intensity_curves(l_gradient, r_gradient):
+  plt.plot(l_gradient, 'g')
+  plt.plot(r_gradient, 'r')
+  plt.ylabel('dI')
+  plt.xlabel('frame number')
+  plt.show()
+
 def main():
+  start = time.time()
   if (len(sys.argv) != 3):
     print('Usage: python3 frame_match_intensity.py <left_feed> <right_feed>')
     exit()
   print 'left feed gradient calculating...'
-  l_gradient = calculate_gradient(sys.argv[1], 0, 25240)
+  l_gradient = calculate_gradient(sys.argv[1])
   print '\nright feed gradient calculating...'
-  r_gradient = calculate_gradient(sys.argv[2], 0, 25240)
+  r_gradient = calculate_gradient(sys.argv[2])
+  plot_intensity_curves(l_gradient, r_gradient)
   opt = get_optimal_offset(l_gradient, r_gradient, 200)
-  print '\nOptimal right feed offset:', opt 
+  print '\nOptimal right feed offset:', opt
+  print 'Time elapsed:', (time.time() - start) / 60, 'minutes'
 
 if __name__ == '__main__':
     main()
