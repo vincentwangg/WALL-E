ea3635108063035f48f4b59771af75b921b5e3b5
vincentwang <vincentwang@umail.ucsb.edu>
2018-04-16 14:55:01 -0700

    Added functionality for animations

--- blender/3d_mapper.py
+++ blender/3d_mapper.py
@@ -4,10 +4,23 @@ import argparse
 import os
 import ast
 
+fps_value = 30
+
 def main(points_filename):
     delete_all_objects()
+    set_fps(fps_value)
     points = get_points_from_file(points_filename)
-    plot_points_from_frame(points, 1)
+
+    for frame_num in points:
+        plot_points_from_frame(points, frame_num)
+
+    # Set the current frame back to the beginning
+    bpy.context.scene.frame_set(0)
+
+
+def set_fps(new_fps):
+    bpy.context.scene.render.fps = new_fps
+    
 
 def get_points_from_file(points_filename):
     # Future iterations file syntax:
@@ -33,23 +46,43 @@ def get_points_from_file(points_filename):
     else:
         sys.exit("File " + points_filename + " couldn't be read.")
 
+
 def plot_points_from_frame(points, frame_num):
     for point in points[frame_num]:
-        plot_point(point)
+        plot_point(point, frame_num)
+
         
 # Coordinates should be in format [x, y, z]
-def plot_point(coordinate):
+def plot_point(coordinate, frame_num):
     bpy.ops.mesh.primitive_ico_sphere_add(size=0.5, location=coordinate)
+    obj = bpy.context.active_object
+
+    obj.keyframe_insert('hide', frame=frame_num)
+    obj.keyframe_insert('hide_render', frame=frame_num)
+    
+    # Hide object
+    obj.hide = True
+    obj.hide_render = True
+    
+    # Hide object in the frames before and after
+    obj.keyframe_insert('hide', frame=frame_num-1)
+    obj.keyframe_insert('hide_render', frame=frame_num-1)
+    obj.keyframe_insert('hide', frame=frame_num+1)
+    obj.keyframe_insert('hide_render', frame=frame_num+1)
+
     
 def select_all_objects():
     bpy.ops.object.select_all(action='SELECT')
+
     
 def deselect_all_objects():
     bpy.ops.object.select_all(action='DESELECT')
+
     
 def delete_all_objects():
     select_all_objects()
     bpy.ops.object.delete()
+
 
 if __name__ == '__main__':
     argv = sys.argv
