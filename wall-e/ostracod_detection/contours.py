8ff428cdd25fb8fdbba8bbe041358a8a76ce7725
wpeery <peery@umail.ucsb.edu>
2018-04-21 10:38:57 -0700

    adds skeleton for pulse matching

--- wall-e/ostracod_detection/contours.py
+++ wall-e/ostracod_detection/contours.py
@@ -20,9 +20,9 @@ im = cv2.imread(args["image"])
 imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 
 ret,thresh = cv2.threshold(imgray,black,white,cv2.THRESH_BINARY)
-imgray, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-# cv2.imshow("imgray",imgray)
-# cv2.waitKey(0)
+imgray2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+cv2.imshow("imgray",imgray)
+cv2.waitKey(0)
 cv2.drawContours(im,contours,2,(0,255,255), -1)
 print "X\tY\tArea\tMinI\tMaxI\tMeanI"
 for c in contours:
@@ -33,9 +33,11 @@ for c in contours:
         cY = int(M["m01"] / M["m00"])
         mask = np.zeros(imgray.shape,np.uint8)
         cv2.drawContours(mask,[c],0,255,-1)
-        mean_val = cv2.mean(im,mask = mask)
+        cv2.imshow("mask", mask)
+        cv2.waitKey(0)
+        mean_val = cv2.mean(imgray,mask = mask)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(imgray,mask = mask)
-        # print cX,"\t",cY,"\t",cv2.contourArea(c),"\t",min_val,"\t",max_val,"\t",mean_val[0]
+        print cX,"\t",cY,"\t",cv2.contourArea(c),"\t",min_val,"\t",max_val,"\t",mean_val[0]
         im[cY,cX] = 0
 
 contour = contours[2]
---
+++ wall-e/ostracod_detection/locating/locator.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+import ostracod
+
+def calculate_location():
+    # calculate (x,y) location of the contour
+    # this means abstracting the contour to a single point
+        # by finding the centroid or something
+
+def calculate_area():
+    # calculate area of a single contour
+    print "calculate area"
+
+def calculate_brightness():
+    # calculate the brightness of a single contour
+    print "calculate brightness"
+
+def find_contours():
+    # returns a list of contours in the image
+    print "find contours"
+
+def filter_image():
+    # filter image
+    # return filtered image
+    print "filter image"
+
+def get_ostracods():
+    # filter image
+    # find contours
+    # compute area of each contour
+    # compute brightness of each contour
+    # compute x, y location of each contour
+    # return list of ostracods
+    print "main"
