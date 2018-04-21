7400ae45605144615e01210f1b923ccce1eba0c6
wpeery <peery@umail.ucsb.edu>
2018-04-21 12:51:46 -0700

    removed some print statements

--- wall-e/ostracod_detection/locating/locator.py
+++ wall-e/ostracod_detection/locating/locator.py
@@ -6,7 +6,6 @@ from ostracod import Ostracod
 
 
 def calculate_location(image, mask):
-    print "calculating loc"
     # calculate (x,y) location of the contour
     # this means abstracting the contour to a single point
     # by finding the centroid or something
@@ -15,26 +14,23 @@ def calculate_location(image, mask):
 
 def calculate_area(contour):
     # calculate area of a single contour
-    print "calculating area"
     return cv2.contourArea(contour)
 
 def calculate_brightness(image, mask):
     # calculate the brightness of a single contour
-    print "calculating brightness"
     brightness = cv2.mean(image, mask=mask)
     return brightness[0]
 
 def find_contours(threshold):
     # returns a list of contours in the image
+    # in the future we may want to vet to eliminate the edge case where multiple ostracods count as 1 contour
     _, contours, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    print "finding contours"
     return contours
 
 def filter_image(image, threshold):
     # filter image
     # return filtered image
     retval, thresh = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY)
-    print "filtering image"
     return thresh
 
 def get_ostracods(filename):
@@ -44,7 +40,7 @@ def get_ostracods(filename):
     # compute brightness of each contour
     # compute x, y location of each contour
     # return list of ostracods
-    print "get ostracods: "
+    print "Ostracods: "
     image = cv2.imread(filename)
     if image is None:
         sys.exit("unable to load image")
@@ -55,8 +51,6 @@ def get_ostracods(filename):
     for c in contours:
         mask = np.zeros(imgray.shape, np.uint8)
         cv2.drawContours(mask, [c], 0, 255, -1)
-        cv2.imshow("mask", mask)
-        cv2.waitKey(0)
         brightness = calculate_brightness(imgray, mask)
         area = calculate_area(c)
         location = calculate_location(imgray, mask)
@@ -64,8 +58,9 @@ def get_ostracods(filename):
             ostracod = Ostracod(location, area, brightness)
             ostracod_list.append(ostracod)
 
+    print "Brightness", "\tArea", "\tLocation"
     for o in ostracod_list:
-        print o.brightness, o.area, o.location
+        print round(o.brightness, 2), "\t\t", o.area, "\t", o.location
     return ostracod_list
 
 
