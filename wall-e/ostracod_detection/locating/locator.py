96be322394ca6ba40b898f2e08037319f821f7f5
wpeery <peery@umail.ucsb.edu>
2018-04-21 12:40:41 -0700

    very basic locator is complete

--- wall-e/ostracod_detection/locating/locator.py
+++ wall-e/ostracod_detection/locating/locator.py
@@ -1,34 +1,77 @@
 import numpy as np
 import cv2
-import ostracod
+import sys
+from ostracod import Ostracod
 
-def calculate_location():
+
+
+def calculate_location(image, mask):
+    print "calculating loc"
     # calculate (x,y) location of the contour
     # this means abstracting the contour to a single point
-        # by finding the centroid or something
+    # by finding the centroid or something
+    _, _, _, max_loc = cv2.minMaxLoc(image, mask=mask)
+    return max_loc
 
-def calculate_area():
+def calculate_area(contour):
     # calculate area of a single contour
-    print "calculate area"
+    print "calculating area"
+    return cv2.contourArea(contour)
 
-def calculate_brightness():
+def calculate_brightness(image, mask):
     # calculate the brightness of a single contour
-    print "calculate brightness"
+    print "calculating brightness"
+    brightness = cv2.mean(image, mask=mask)
+    return brightness[0]
 
-def find_contours():
+def find_contours(threshold):
     # returns a list of contours in the image
-    print "find contours"
+    _, contours, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    print "finding contours"
+    return contours
 
-def filter_image():
+def filter_image(image, threshold):
     # filter image
     # return filtered image
-    print "filter image"
+    retval, thresh = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY)
+    print "filtering image"
+    return thresh
 
-def get_ostracods():
+def get_ostracods(filename):
     # filter image
     # find contours
     # compute area of each contour
     # compute brightness of each contour
     # compute x, y location of each contour
     # return list of ostracods
-    print "main"
+    print "get ostracods: "
+    image = cv2.imread(filename)
+    if image is None:
+        sys.exit("unable to load image")
+    imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    threshold = filter_image(imgray, 120)
+    contours = find_contours(threshold)
+    ostracod_list = []
+    for c in contours:
+        mask = np.zeros(imgray.shape, np.uint8)
+        cv2.drawContours(mask, [c], 0, 255, -1)
+        cv2.imshow("mask", mask)
+        cv2.waitKey(0)
+        brightness = calculate_brightness(imgray, mask)
+        area = calculate_area(c)
+        location = calculate_location(imgray, mask)
+        if area >= 5:
+            ostracod = Ostracod(location, area, brightness)
+            ostracod_list.append(ostracod)
+
+    for o in ostracod_list:
+        print o.brightness, o.area, o.location
+    return ostracod_list
+
+
+def main():
+    get_ostracods('../ostracod.png')
+
+
+if __name__ == '__main__':
+    main()
--- wall-e/ostracod_detection/locating/ostracod.py
+++ wall-e/ostracod_detection/locating/ostracod.py
@@ -1,8 +1,8 @@
 # Ostracod Model
 
-class ostracod:
-    def __init__(self):
-        self.location = []      # x, y, z coordinates
-        self.area               # area of the contour
-        self.brightness         # 0-255
-        self.matched            # coordinates of another ostracod in a corresponding stereo frame
+class Ostracod:
+    def __init__(self, location, area, brightness, matched=False):
+        self.location = location     # x, y coordinates
+        self.area = area              # area of the contour
+        self.brightness = brightness         # 0-255
+        self.matched = matched           # coordinates of another ostracod in a corresponding stereo frame
