a68a8b2a4b7592c6e3f2f4df08674d3172d61b1e
vincentwang <vincentwang@umail.ucsb.edu>
2018-04-23 14:46:25 -0700

    Created video_frame_loader

---
+++ wall-e/video_frame_loader.py
@@ -0,0 +1,15 @@
+import cv2
+
+
+class VideoFrameLoader:
+    def __init__(self, left_feed_filename, right_feed_filename):
+        self.vc_left = cv2.VideoCapture(left_feed_filename)
+        self.vc_right = cv2.VideoCapture(right_feed_filename)
+
+    def get_left_frame(self, frame_num):
+        self.vc_left.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+        return self.vc_left.read()
+
+    def get_right_frame(self, frame_num):
+        self.vc_right.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+        return self.vc_right.read()
