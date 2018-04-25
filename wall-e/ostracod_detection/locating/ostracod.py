de6be57f2ff2d96591d09cdbb4139c75fed08e7c
wpeery <peery@umail.ucsb.edu>
2018-04-24 18:29:17 -0700

    Finished basic matching algorithm. Tested it on two identical images 100% match, still have to test on two different images

--- wall-e/ostracod_detection/locating/ostracod.py
+++ wall-e/ostracod_detection/locating/ostracod.py
@@ -1,8 +1,8 @@
 # Ostracod Model
 
 class Ostracod:
-    def __init__(self, location, area, brightness, matched=False):
+    def __init__(self, location, area, brightness):
         self.location = location     # x, y coordinates
         self.area = area              # area of the contour
         self.brightness = brightness         # 0-255
-        self.matched = matched           # coordinates of another ostracod in a corresponding stereo frame
+        self.matches = []           # indexes of another ostracod in a corresponding stereo frame
