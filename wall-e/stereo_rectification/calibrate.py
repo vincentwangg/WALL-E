8ae6d5bad50c619d1ece3cd37d67adb3abb064e8
vincentwang <vincentwang@umail.ucsb.edu>
2018-04-25 21:57:19 -0700

    Revamped calibrate.py

--- wall-e/stereo_rectification/calibrate.py
+++ wall-e/stereo_rectification/calibrate.py
@@ -4,28 +4,21 @@
 import cv2
 assert cv2.__version__[0] == '3', 'The fisheye module requires opencv version >= 3.0.0'
 import numpy as np
-
-CHECKERBOARD = (6, 8)
-subpix_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
-calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC + cv2.fisheye.CALIB_CHECK_COND + cv2.fisheye.CALIB_FIX_SKEW
-objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
-objp[0, :, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
-_img_shape = None
-objpoints = []  # 3d point in real world space
-imgpoints = []  # 2d points in image plane.
-
-vc_obj_left = cv2.VideoCapture("../Left.mkv")
-vc_obj_right = cv2.VideoCapture("../Right.mkv")
+import argparse
 
 
-def run_through_video(vid_capture_obj, begin_idx, end_idx_exc):
+def run_through_video(vid_capture_obj):
     global _img_shape, gray
     successes = 0
-    for i in range(begin_idx, end_idx_exc):
-        print(i)
-        vid_capture_obj.set(cv2.CAP_PROP_POS_FRAMES, i)
+    vid_capture_obj.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+    while True:
         vc_obj_right_success, img = vid_capture_obj.read()
-        if _img_shape == None:
+
+        if not vc_obj_right_success:
+            return
+
+        if _img_shape is None:
             _img_shape = img.shape[:2]
         else:
             assert _img_shape == img.shape[:2], "All images must share the same size."
@@ -33,43 +26,56 @@ def run_through_video(vid_capture_obj, begin_idx, end_idx_exc):
 
         # Find the chess board corners
         ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD,
-                                                 cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+                                                 cv2.CALIB_CB_ADAPTIVE_THRESH +
+                                                 cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
 
         # If found, add object points, image points (after refining them)
-        if ret == True:
+        if ret:
             objpoints.append(objp)
             cv2.cornerSubPix(gray, corners, (3, 3), (-1, -1), subpix_criteria)
             imgpoints.append(corners)
             successes += 1
             print("Success: " + str(successes))
-            if successes == 100:
-                break
         else:
             print("Fail. Current successes: " + str(successes))
 
 
-run_through_video(vc_obj_left, 731, 900)
-run_through_video(vc_obj_right, 731, 900)
-N_OK = len(objpoints)
-K = np.zeros((3, 3))
-D = np.zeros((4, 1))
-rvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
-tvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
-rms, _, _, _, _ = \
-    cv2.fisheye.calibrate(
-        objpoints,
-        imgpoints,
-        gray.shape[::-1],
-        K,
-        D,
-        rvecs,
-        tvecs,
-        calibration_flags,
-        (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6)
-    )
-print("Found " + str(N_OK) + " valid images for calibration")
-print("DIM=" + str(_img_shape[::-1]))
-print("K=np.array(" + str(K.tolist()) + ")")
-print("D=np.array(" + str(D.tolist()) + ")")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("video_filename", help="filename of the video feed")
+    args = parser.parse_args()
 
-cv2.waitKey()
+    CHECKERBOARD = (6, 8)
+    subpix_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
+    calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC + cv2.fisheye.CALIB_CHECK_COND + cv2.fisheye.CALIB_FIX_SKEW
+    objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
+    objp[0, :, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+    _img_shape = None
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
+
+    vc_obj = cv2.VideoCapture(args.video_filename)
+
+    run_through_video(vc_obj)
+
+    N_OK = len(objpoints)
+    K = np.zeros((3, 3))
+    D = np.zeros((4, 1))
+    rvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
+    tvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
+    rms, _, _, _, _ = \
+        cv2.fisheye.calibrate(
+            objpoints,
+            imgpoints,
+            gray.shape[::-1],
+            K,
+            D,
+            rvecs,
+            tvecs,
+            calibration_flags,
+            (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6)
+        )
+    print("Found " + str(N_OK) + " valid images for calibration")
+    print("DIM=" + str(_img_shape[::-1]))
+    print("K=np.array(" + str(K.tolist()) + ")")
+    print("D=np.array(" + str(D.tolist()) + ")")
