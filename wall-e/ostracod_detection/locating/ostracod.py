e356c36eb08ef822f2b4d7c675809e1514d65f96
wpeery <peery@umail.ucsb.edu>
2018-05-07 20:23:15 -0700

    update ostracod model to remove distance to mean

--- wall-e/ostracod_detection/locating/ostracod.py
+++ wall-e/ostracod_detection/locating/ostracod.py
@@ -6,5 +6,3 @@ class Ostracod:
         self.area = area                    # area of the contour
         self.brightness = brightness        # 0-255
         self.matches = []                   # indexes of another ostracod in a corresponding stereo frame, match value
-        self.distance_from_mean = 0         # x distance away from mean x coordinate
-                                                # (this gets updated in locator.get_ostracods())
