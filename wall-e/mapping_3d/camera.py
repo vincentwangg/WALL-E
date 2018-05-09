7d8002beee579eb85735a1af26f31de7550f63ae
wpeery <peery@umail.ucsb.edu>
2018-05-09 00:02:34 -0700

    Created mapper_3d_test. Improved mapper_3d. Improved camera by adding optional argument for focal length.

--- wall-e/mapping_3d/camera.py
+++ wall-e/mapping_3d/camera.py
@@ -1,10 +1,13 @@
 import cv2
 
 class Camera:
-    def __init__(self, baseline):
-        self.focal_length = self.get_focal_length()
+    def __init__(self, baseline, focal_length=None):
+        if focal_length is None:
+            self.focal_length = self.get_focal_length()
+        else:
+            self.focal_length = focal_length
         self.baseline = baseline
 
 
-    def get_focal_length(self):
-        print "stub"
+    def get_focal_length(self): # needs video and access to sr_map
+        return 3.7
--- wall-e/mapping_3d/mapper_3d.py
+++ wall-e/mapping_3d/mapper_3d.py
@@ -1,37 +1,40 @@
 import math
 from pulse_data import FramePulseData
 from pulse_data import PulseData
+from camera import Camera
 import sys
 
 
-def depth_map(ostracod_list1, ostracod_list2, fpd, framenum):
-    if not isinstance(fpd, FramePulseData):
+def depth_map(ostracod_list1, ostracod_list2, framepulsedata, framenum, camera):
+    if not isinstance(framepulsedata, FramePulseData):
         sys.exit("fpd must be of type FramePulseData")
+    if not isinstance(camera, Camera):
+        sys.exit("camera must be of type Camera")
     for o in ostracod_list1:
         if len(o.matches) > 0:
-            pd = generate_pulse_data(o, ostracod_list2[o.matches[0][0]])
-            fpd.add_pulse_to_frame(framenum, pd)
+            pd = generate_pulse_data(o, ostracod_list2[o.matches[0][0]], camera)
+            framepulsedata.add_pulse_to_frame(framenum, pd)
 
 
-def generate_pulse_data(ostracod1, ostracod2):
+def generate_pulse_data(ostracod1, ostracod2, camera):
     avg_brightness = (ostracod1.brightness + ostracod2.brightness)/2
     avg_area = (ostracod1.area + ostracod2.area)/2
-    location = [ostracod1.location[0], ostracod1.location[1]]
-    z = 0 # to be get_x_Val() or something
-    location.append(z)
-    brightness = scale_attribute(avg_brightness, z)
-    area = scale_attribute(avg_area, z)
+
+    z = get_z_val()
+
+    real_coordinate_ratio = z/camera.focal_length
+    location = [ostracod1.location[0]*real_coordinate_ratio, ostracod2.location[1]*real_coordinate_ratio, z]
+
+    brightness = scale_2d_attribute(avg_brightness, z)
+    area = scale_2d_attribute(avg_area, z)
     radius = (area/math.pi)**0.5
     return PulseData(xyz_coord=location, radius=radius, brightness=brightness)
 
 
-def scale_attribute(attribute, z_val):
+def get_z_val():
+    return 1
+
+
+def scale_2d_attribute(attribute, z_val):
     return attribute*(z_val+1)**2
 
-
-def main():
-    print "stub"
-
-
-if __name__ == '__main__':
-    main()
