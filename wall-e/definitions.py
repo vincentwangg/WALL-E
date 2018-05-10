a7973ae2ccfcbd01fe44ebd738cfbe37817329ea
wpeery <peery@umail.ucsb.edu>
2018-05-09 19:35:07 -0700

    Fixed error in PROJECT_ROOT_PATH

--- wall-e/definitions.py
+++ wall-e/definitions.py
@@ -2,4 +2,4 @@ import os
 
 SRC_PATH = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH = os.path.join(SRC_PATH, "config.txt")
-PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath("../"))
+PROJECT_ROOT_PATH = os.path.abspath(os.path.join(SRC_PATH, os.pardir))
