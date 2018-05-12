b51b7562df4a11e262deb241e278364c1753d067
Karthik Kribakaran <karthik.kriba@gmail.com>
2018-05-12 15:05:13 -0700

    add pixel width/height scaling to 3d mapping

--- wall-e/mapping_3d/mapper_3d.py
+++ wall-e/mapping_3d/mapper_3d.py
@@ -18,13 +18,11 @@ def depth_map(ostracod_list1, ostracod_list2, framepulsedata, framenum, camera):
 
 
 def generate_pulse_data(ostracod1, ostracod2, camera):
-    avg_brightness = (ostracod1.brightness + ostracod2.brightness)/2
-    avg_area = (ostracod1.area + ostracod2.area)/2
+    avg_brightness = (ostracod1.brightness + ostracod2.brightness) / 2
+    avg_area = (ostracod1.area + ostracod2.area) / 2
 
-    z = get_z_val(ostracod1.location[0], ostracod2.location[0], camera.focal_length, camera.baseline)
-
-    real_coordinate_ratio = z/camera.focal_length
-    location = [ostracod1.location[0]*real_coordinate_ratio, ostracod2.location[1]*real_coordinate_ratio, z]
+    location = get_coord(ostracod1.location, ostracod2.location, camera.focal_length, camera.baseline)
+    z = location[2]
 
     brightness = scale_2d_attribute(avg_brightness, z)
     area = scale_2d_attribute(avg_area, z)
@@ -32,8 +30,21 @@ def generate_pulse_data(ostracod1, ostracod2, camera):
     return PulseData(xyz_coord=location, radius=radius, brightness=brightness)
 
 
-def get_z_val(left_x, right_x, focal_length, baseline):
-    return focal_length * baseline / (left_x - right_x)
+def get_coord(left_coord, right_coord, focal_length, baseline):
+    # hardcode pixel widths
+    pixel_width = 0.0084 #mm
+    pixel_height = 0.0098 #mm
+
+    X_L = left_coord[0]
+    X_R = right_coord[0]
+    z = focal_length * baseline / (float(X_L - X_R))
+
+    x_l = X_L * pixel_width
+    x_r = X_R * pixel_width
+    y_l = Y_L * pixel_height
+    y_r = Y_R * pixel_height
+    
+    return [x_l * z / focal_length, y_l * z / focal_length, z]
 
 
 def scale_2d_attribute(attribute, z_val):
