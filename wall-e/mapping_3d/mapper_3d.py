c187c656d306a9bb355a4e6648e7b9d0bd92d571
Karthik Kribakaran <karthik.kriba@gmail.com>
2018-05-22 21:12:01 -0700

    fix None error

--- wall-e/mapping_3d/mapper_3d.py
+++ wall-e/mapping_3d/mapper_3d.py
@@ -49,6 +49,6 @@ def gen_frame_pulse_data(matched_ostracod_list, frame_pulse_data):
         delt_r = (init_r - THRESH_R) / o.num_frames
         num_frames = int(init_r / delt_r)
         for x in range(num_frames):
-        frame_pulse_data.add_pulse_to_frame(o.frame_start + x,
+            frame_pulse_data.add_pulse_to_frame(o.frame_start + x,
                          PulseData(o.location, init_r - (x * delt_r), brightness=1))
 
