699ae0d87a97b1c33f8b17f64c987eba3a082e48
vincentwang <vincentwang@umail.ucsb.edu>
2018-05-28 23:14:13 -0700

    Pulse data now gets written to the correct file

--- wall-e/blender_script.py
+++ wall-e/blender_script.py
@@ -1,11 +1,12 @@
 import os
-from definitions import ROOT_DIR
 import subprocess
+
+from definitions import ROOT_DIR
 
 
 def main():
     os.chdir(ROOT_DIR)
-    subprocess.check_call(["blender", "--python", "mapping_3d/blender_pulse_mapper.py", "--"])
+    subprocess.call(["blender", "--python mapping_3d/blender_pulse_mapper.py"])
 
 
 if __name__ == '__main__':
--- wall-e/definitions.py
+++ wall-e/definitions.py
@@ -4,8 +4,12 @@ TMP_FOLDER_NAME = "tmp"
 STEREO_RECTIFICATION_DIR_NAME = "stereo_rectification"
 MAPPING_3D_DIR_NAME = "mapping_3d"
 
+# Root directory is ~/WALL-E/wall-e (Where all the source code is)
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Project directory is ~/WALL-E (Where all the project files are)
 PROJECT_DIR = os.path.abspath(os.path.join(ROOT_DIR, os.pardir))
+
 TMP_FOLDER_PATH = os.path.abspath(os.path.join(PROJECT_DIR, TMP_FOLDER_NAME))
 CONFIG_PATH = os.path.join(ROOT_DIR, ".keybindings.cfg")
 GUI_CACHE_PATH = os.path.join(ROOT_DIR, ".gui_cache.txt")
