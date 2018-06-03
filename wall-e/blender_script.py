b7c9d271935b32288053d9c9459a5caa36131c09
wpeery <peery@umail.ucsb.edu>
2018-06-02 23:19:18 -0700

    blender_script now gets blender path

--- wall-e/blender_script.py
+++ wall-e/blender_script.py
@@ -6,7 +6,9 @@ from definitions import ROOT_DIR
 
 def main():
     os.chdir(ROOT_DIR)
-    subprocess.call(["blender", "--python", "mapping_3d/blender_pulse_mapper.py"])
+    out = subprocess.check_output(["whereis", "blender"])
+    blender_path = out.split()[1]
+    subprocess.call([blender_path, "--python", "mapping_3d/blender_pulse_mapper.py"])
 
 
 if __name__ == '__main__':
