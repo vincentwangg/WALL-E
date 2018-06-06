0655c556914b08d818cf4dc63e4ce6923b5383cc
wpeery <peery@umail.ucsb.edu>
2018-06-06 11:07:45 -0700

    update old pipeline to be consistent with the new

--- wall-e/pipeline2_old.py
+++ wall-e/pipeline2_old.py
@@ -12,23 +12,29 @@ import blender_script
 import time
 import argparse
 from utils_general.file_checker import check_if_file_exists
+from utils_general.text_progress_bar import print_progress
+import cv2
 
 def main():
     parser = argparse.ArgumentParser(description="Reads in left and right videos as well as the camera baseline.")
     parser.add_argument("left_video", help="filename of the left video feed", type=str)
     parser.add_argument("right_video", help="filename of the right video feed", type=str)
     parser.add_argument("baseline", help="The distance between the two stereo cameras", type=float)
+    parser.add_argument("-y", "--yaml_file", default=None, type=str,
+                        help="Filename of the yaml file that contains the stereo rectification maps. Default is None")
+
     args = parser.parse_args()
 
     left_file_name = args.left_video
     right_file_name = args.right_video
     baseline = args.baseline
+    sr_yml_file_name = args.yaml_file
 
     check_if_file_exists(left_file_name)
     check_if_file_exists(right_file_name)
 
     vfl = VideoFrameLoader(left_file_name, right_file_name)
-    camera = Camera(baseline=baseline, focal_length=515)
+    camera = Camera(baseline=baseline, sr_yml_filename=sr_yml_file_name)
     fpd = FramePulseData()
 
     success_r, right_image = vfl.get_next_right_frame()
@@ -38,8 +44,12 @@ def main():
     match_time = 0
     depth_map_time = 0
     reading_vid_time = 0
+
+    vid = cv2.VideoCapture(left_file_name)
+    total_frames = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+
     while success_l and success_r:
-        print frame_num
+        print_progress(frame_num, total_frames, 'Generating Text File: ', 'Complete', 1, 50)
         locate_start = time.time()
         ostracod_list_l = locator.get_ostracods(left_image)
         ostracod_list_r = locator.get_ostracods(right_image)
