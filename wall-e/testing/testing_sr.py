50f4a86a4329d679fc608b9a4e527dc6a6eedde0
wpeery <peery@umail.ucsb.edu>
2018-09-26 14:53:11 -0700

    adds script that combines two stereo rectified videos and draws horizontal lines so they are easier to compare

--- wall-e/testing/testing_sr.py
+++ wall-e/testing/testing_sr.py
@@ -1,47 +1,85 @@
+import argparse
+from utils_general.file_checker import check_if_file_exists
 import cv2
-import stereo_rectification
-
-
-def get_images(frame_num, offset, left, right):
-    left.set(cv2.CAP_PROP_POS_FRAMES,frame_num)
-    right.set(cv2.CAP_PROP_POS_FRAMES,frame_num + offset)
-    suc, l_image = left.read()
-    suc, r_image = right.read()
-    return l_image, r_image
-
-
-def show(left, right):
-    cv2.imshow("left", left)
-    cv2.imshow("right", right)
-    cv2.waitKey(0)
-
-def get_f(video):
-    video.set(cv2.CAP_PROP_POS_FRAMES,2700)
-    succ, image = video.read()
-    image[260,306] = 255
-    cv2.imshow("og_image",image)
-    cv2.waitKey(0)
+import numpy as np
 
 def main():
-    frame_num = 1650
-    offset = 13
-    org_left = cv2.VideoCapture('../Flat_rightview.mkv')
-    left = cv2.VideoCapture('../Flat_leftview_stereo_rectified.mkv')
-    right = cv2.VideoCapture('../Flat_rightview_stereo_rectified.mkv')
-    left_image, right_image = get_images(frame_num, offset, left, right)
-    for line in range(0, int(left_image.shape[0] / 20)):
-        left_image[line * 20, :] = 0,0,255
-        right_image[line * 20, :] = 0,0,255
-    # cv2.cvtColor(left_image,left_image, cv2.COLOR_GRAY2RGB)
-    # cv2.cvtColor(right_image, right_image, cv2.COLOR_GRAY2RGB)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("left_video_file_name", help="filename of the left video feed")
+    parser.add_argument("right_video_file_name", help="filename of the right video feed")
 
-    right_image[260, 314] = 255
-    left_image[260, 381] = 255
-    show(left_image, right_image)
-    # get_f(org_left)
-    # left image is 351
-    # right image is 314
+    args = parser.parse_args()
 
+    if (args_are_correct(args)):
+        images = generate_images_with_horizontal_lines(args.left_video_file_name, args.right_video_file_name)
+        create_video_from_frame_list(images)
+        print "Done!!"
+
+
+def generate_images_with_horizontal_lines(left_video_file_name, right_video_file_name):
+    left_video = cv2.VideoCapture(left_video_file_name)
+    right_video = cv2.VideoCapture(right_video_file_name)
+
+    left_image_success, left_image = left_video.read()
+    right_image_success, right_image = right_video.read()
+
+    output_images = []
+    
+    while(left_image_success and right_image_success):
+        write_horizontal_lines(left_image)
+        write_horizontal_lines(right_image)
+        output_images.append(combine_two_frames(left_image, right_image))
+
+        left_image_success, left_image = left_video.read()
+        right_image_success, right_image = right_video.read()
+
+    return output_images
+
+
+def create_video_from_frame_list(image_frame_list):
+    if len(image_frame_list) > 0:
+        new_file_name = "../videos/testing_sr.mkv"
+        fourcc = cv2.VideoWriter_fourcc(*'FFV1')
+        fps = 30.0
+        width = image_frame_list[0].shape[1]
+        height = image_frame_list[0].shape[0]
+
+        test_video = cv2.VideoWriter(new_file_name, fourcc, fps, (width, height))
+        for frame in image_frame_list:
+            test_video.write(frame)
+        print "saved video: " + new_file_name
+    else:
+        print "image_frame_list was empty"
+
+
+def combine_two_frames(left_image, right_image):
+    left_shape = left_image.shape
+    right_shape = right_image.shape
+
+    left_width = left_shape[1]
+    left_height = left_shape[0]
+    
+    right_width = right_shape[1]
+    right_height = right_shape[0]
+
+    combined_image = np.zeros([left_height, left_width + right_width, 3], dtype=np.uint8)
+
+    combined_image[:left_height, :left_width, :] = left_image
+    combined_image[:left_height, left_width:left_width + right_width, :] = right_image
+
+    return combined_image
+
+
+
+def write_horizontal_lines(image):
+    for line in range(0, int(image.shape[0] / 20)):
+        image[line * 20, :] = 255,255,255
+
+
+def args_are_correct(args):
+    check_if_file_exists(args.left_video_file_name)
+    check_if_file_exists (args.right_video_file_name)
+    return True
 
 
 if __name__ == '__main__':
